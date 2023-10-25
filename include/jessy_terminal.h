c48ec27ab7cbe4d32f6ec14cc76e456446a06b9e
nthnn <nathanneisip@gmail.com>
2023-10-25 19:54:46 +0800

    New terminal commands: mkdir. And some fixes

--- include/jessy_terminal.h
+++ include/jessy_terminal.h
@@ -3,34 +3,34 @@
 
 class JessyTerminal {
 public:
-    static void cd(JessyAgent agent, String arguments[], uint8_t argc);
-    static void pwd(JessyAgent agent, String arguments[], uint8_t argc);
-    static void ls(JessyAgent agent, String arguments[], uint8_t argc);
-    static void mkdir(JessyAgent agent, String arguments[], uint8_t argc);
-    static void touch(JessyAgent agent, String arguments[], uint8_t argc);
-    static void rm(JessyAgent agent, String arguments[], uint8_t argc);
-    static void cp(JessyAgent agent, String arguments[], uint8_t argc);
-    static void mv(JessyAgent agent, String arguments[], uint8_t argc);
+    static void cd(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void pwd(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void ls(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void mkdir(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void touch(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void rm(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void cp(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void mv(JessyAgent &agent, String arguments[], uint8_t argc);
 
-    static void cat(JessyAgent agent, String arguments[], uint8_t argc);
-    static void echo(JessyAgent agent, String arguments[], uint8_t argc);
+    static void cat(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void echo(JessyAgent &agent, String arguments[], uint8_t argc);
 
-    static void useradd(JessyAgent agent, String arguments[], uint8_t argc);
-    static void userdel(JessyAgent agent, String arguments[], uint8_t argc);
-    static void passwd(JessyAgent agent, String arguments[], uint8_t argc);
-    static void sys(JessyAgent agent, String arguments[], uint8_t argc);
-    static void su(JessyAgent agent, String arguments[], uint8_t argc);
+    static void useradd(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void userdel(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void passwd(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void sys(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void su(JessyAgent &agent, String arguments[], uint8_t argc);
 
-    static void sd(JessyAgent agent, String arguments[], uint8_t argc);
-    static void esp32cpu(JessyAgent agent, String arguments[], uint8_t argc);
+    static void sd(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void esp32cpu(JessyAgent &agent, String arguments[], uint8_t argc);
 
-    static void date(JessyAgent agent, String arguments[], uint8_t argc);
-    static void time(JessyAgent agent, String arguments[], uint8_t argc);
-    static void cal(JessyAgent agent, String arguments[], uint8_t argc);
+    static void date(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void time(JessyAgent &agent, String arguments[], uint8_t argc);
+    static void cal(JessyAgent &agent, String arguments[], uint8_t argc);
 
-    static void js(JessyAgent agent, String arguments[], uint8_t argc);
+    static void js(JessyAgent &agent, String arguments[], uint8_t argc);
 };
 
-void JessyExecCommand(JessyAgent agent, String arguments[], uint8_t argc);
+void JessyExecCommand(JessyAgent &agent, String arguments[], uint8_t argc);
 
 #endif
--- src/jessy_terminal.cpp
+++ src/jessy_terminal.cpp
@@ -4,24 +4,24 @@
 #include <jessy_util.h>
 
 static void printCommandError(String cmd, String message) {
-    JessyUtility::log(JSY_LOG_ERROR, cmd + ": " + message);
+    JessyUtility::log(JSY_LOG_ERROR, cmd + F(": ") + message);
 }
 
 static void printIncorrectArity(String cmd) {
-    printCommandError(cmd, "Incorrect parameter arity.");
+    printCommandError(cmd, F("Incorrect parameter arity."));
 }
 
-void JessyTerminal::cd(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::cd(JessyAgent &agent, String arguments[], uint8_t argc) {
     if(argc != 2) {
         printIncorrectArity(arguments[0]);
         return;
     }
 
     if(!agent.setWorkingDirectory(arguments[1]))
-        printCommandError("cd", "Cannot change working directory.");
+        printCommandError("cd", F("Cannot change working directory."));
 }
 
-void JessyTerminal::pwd(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::pwd(JessyAgent &agent, String arguments[], uint8_t argc) {
     if(argc != 1) {
         printIncorrectArity(arguments[0]);
         return;
@@ -30,37 +30,64 @@ void JessyTerminal::pwd(JessyAgent agent, String arguments[], uint8_t argc) {
     JessyIO::println(agent.getWorkingDirectory());
 }
 
-void JessyTerminal::ls(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::ls(JessyAgent &agent, String arguments[], uint8_t argc) {
     bool isRecursive = false, includeHiddenFiles = false;
     for(uint8_t i = 1; i < argc; i++) {
         String arg = arguments[i];
 
-        if(arg == "-r")
+        if(arg == F("-r"))
             isRecursive = true;
-        else if(arg == "-a")
+        else if(arg == F("-a"))
             includeHiddenFiles = true;
+    }
+
+    String files[300];
+    uint16_t count;
+
+    if(isRecursive)
+        count = JessyIO::listFilesRecursive(agent.getWorkingDirectory(), files);
+    else count = JessyIO::listFiles(agent.getWorkingDirectory(), files);
+
+    for(uint16_t i = 0; i < count; i++) {
+        String fileName = files[i];
+        if(!includeHiddenFiles && fileName.startsWith(F("."))) 
+            continue;
+
+        JessyRTC lastWrite = JessyIO::getLastWrite(agent.getWorkingDirectory() + F("/") + fileName);
+        JessyIO::print("[" + JessyUtility::rtcStructString(lastWrite) + "]\t");
+        JessyIO::println(fileName);
     }
 }
 
-void JessyTerminal::mkdir(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::mkdir(JessyAgent &agent, String arguments[], uint8_t argc) {
+    if(argc < 2) {
+        printIncorrectArity(arguments[0]);
+        return;
+    }
+
+    for(uint8_t i = 1; i < argc; i++) {
+        String dir = arguments[i];
+        if(!JessyIO::mkdir(agent.getWorkingDirectory() + F("/") + dir))
+            printCommandError(arguments[0], "Cannot create directory: " + dir);
+    }
 }
 
-void JessyTerminal::touch(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::touch(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::rm(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::rm(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::cp(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::cp(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::mv(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::mv(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::cat(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::cat(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::echo(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::echo(JessyAgent &agent, String arguments[], uint8_t argc) {
     if(argc < 2) {
         printIncorrectArity(arguments[0]);
         return;
@@ -70,50 +97,49 @@ void JessyTerminal::echo(JessyAgent agent, String arguments[], uint8_t argc) {
         JessyIO::println(arguments[i]);
 }
 
-void JessyTerminal::useradd(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::useradd(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::userdel(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::userdel(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::passwd(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::passwd(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::sys(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::sys(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::su(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::su(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::sd(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::sd(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::esp32cpu(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::esp32cpu(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::date(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::date(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::time(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::time(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::cal(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::cal(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyTerminal::js(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyTerminal::js(JessyAgent &agent, String arguments[], uint8_t argc) {
 }
 
-void JessyExecCommand(JessyAgent agent, String arguments[], uint8_t argc) {
+void JessyExecCommand(JessyAgent &agent, String arguments[], uint8_t argc) {
     String cmd = arguments[0];
 
     #define JSY_EXEC(cmd) JessyTerminal:: cmd (agent, arguments, argc);
 
-    if(cmd == "cd")
-        JSY_EXEC(cd)
-    else if(cmd == "pwd")
-        JSY_EXEC(pwd)
-    else if(cmd == "echo")
-        JSY_EXEC(echo)
+    if(cmd == F("cd"))          JSY_EXEC(cd)
+    else if(cmd == F("pwd"))    JSY_EXEC(pwd)
+    else if(cmd == F("ls"))     JSY_EXEC(ls)
+    else if(cmd == F("mkdir"))  JSY_EXEC(mkdir)
+    else if(cmd == F("echo"))   JSY_EXEC(echo)
     else JessyUtility::log(
         JSY_LOG_ERROR,
         "Command not found: " + arguments[0]
